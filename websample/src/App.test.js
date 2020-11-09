34a0d8ba3e403c34b06fdac0e8328a22e40d190a
YvesCheung <975135274@qq.com>
2020-11-09 16:07:51 +0800

    Line 3:6:  should not have duplicate prefix  jest/valid-title

--- websample/src/App.test.js
+++ websample/src/App.test.js
@@ -1,6 +1,6 @@
 import EmojiReader from 'emoji-reader'
 
-test('test emoji reader', () => {
+test('emoji reader', () => {
     var text = "a😀ash😕s🙁sad☹️sd😣s😖asd😫sd😩"
     console.log(EmojiReader.isEmojiOfCharIndex(text, text.length - 1))
     console.log(EmojiReader.transToUnicode(text))
